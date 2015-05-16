743a8095957c15b1dc65a05620683e47d4d627ad
Christopher Engelbrecht <chris.engelbrecht23@gmail.com>
2015-05-16 14:43:35 -0700

    modified controller

--- Python Drive/Controller.py
+++ Python Drive/Controller.py
@@ -1,9 +1,26 @@
 import from PID *
+import math
 
-class zControl():
-    def __init__(self, desired=0):
-        self.desired = desired
-        self.pid = PID(Kp=2, Kd=3, Ki=0.5)
+# import note for the math: 
+#   - z is the axis pointing out of the dock
+#   - y is height
+#   - x is right left
+#   - r is the radius from the dock
+#   - gamma is the angle of the ROV relative to the dock
+
+class Controller():
+    def __init__(self, x, y, z):
+
+        # control the radius from the dock,
+        # the height of the ROV relative to the dock,
+        # and the angle of the ROV relative to the dock (between pi and -pi)
+        self.rDesired = math.sqrt(x**2 + z**2)
+        self.yDesired = y
+        self.gammaDesired = math.atan2(x, z)
+
+        self.rPid = PID(Kp=2, Kd=3, Ki=0.5)
+        self.yPid = PID(Kp=2, Kd=3, Ki=0.5)
+        self.gammaPid  = PID(Kp=2, Kd=3, Ki=0.5)
 
     def setDesired(self, desired):
         self.desired = desired
